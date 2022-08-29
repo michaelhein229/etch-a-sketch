d98b75b86dcdd7e2cb21b553e709d2836f72d6a6
michaelhein229 <michaelhein229@gmail.com>
2022-08-28 21:02:41 -0700

    worked on some aesthetics and added a non functional grid slider

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,8 @@
 
-let gridSize = 16;
 let container = document.querySelector(".container");
+let userInputs = document.getElementById("user-inputs");
+let sliderValue = document.getElementById("dimensions")
+let gridSize = 16;
 
 const colorPicker = document.getElementById("color-picker")
 let color2 = colorPicker.value;
@@ -14,7 +16,7 @@ class Square {
     this.color = color;
     this.square = document.createElement("div");
     const square = this.square
-    square.style.cssText = `background-color: ${this.color}; height: ${750 / gridSize}px; width: ${750 / gridSize}px;`
+    square.style.cssText = `background-color: ${this.color}; height: ${750 / gridSize}px; width: ${750 / gridSize}px; border: .1px solid black;`
     container.appendChild(square)
 
     square.addEventListener("mouseover", function() {
