f5e8a649fdf45030e6ef27840f19f1192e3787d1
Michael Hein <michaelhein229@gmail.com>
2022-09-05 00:22:16 -0700

    Grid Scales decently. Can clear board and fill it with the selected color. Grid size also indicated below grid slider!

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,38 @@
+let squareList = [];
+/* Create a Square object with color, and background color
+   and append it to its container also add an EventListener
+   to change its background color on hover                */
+   class Square {
+    constructor(color) {
+      this.color = color;
+      this.square = document.createElement("div");
+      const square = this.square
+      square.style.cssText = `background-color: ${this.color}; height: ${750 / gridSize}px; width: ${750 / gridSize}px; border: .1px solid black;`
+      container.appendChild(square)
+  
+      square.addEventListener("mouseover", function() {
+        square.style.backgroundColor = color2;
+      }) 
+      
+      squareList.push(this.square);
+    }
+  }
+
 
 let container = document.querySelector(".container");
-let userInputs = document.getElementById("user-inputs");
-let sliderValue = document.getElementById("dimensions")
+let slider = document.getElementById("dimensions");
+let sliderValue = document.getElementById("number");
 let gridSize = 16;
+slider.value = gridSize;
+renderGrid(gridSize);
+
+
+slider.addEventListener("input", function() {
+  gridSize = slider.value;
+  sliderValue.innerText = gridSize;
+  renderGrid(gridSize);
+})
+
 
 const colorPicker = document.getElementById("color-picker")
 let color2 = colorPicker.value;
@@ -11,25 +41,33 @@ colorPicker.addEventListener("input", function() {
 })
 
 
-class Square {
-  constructor(color) {
-    this.color = color;
-    this.square = document.createElement("div");
-    const square = this.square
-    square.style.cssText = `background-color: ${this.color}; height: ${750 / gridSize}px; width: ${750 / gridSize}px; border: .1px solid black;`
-    container.appendChild(square)
 
-    square.addEventListener("mouseover", function() {
-      square.style.backgroundColor = color2;
-    }) 
-  }
-}
-
-container.style.cssText = "grid-template-columns: auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto;"
-
-
+function renderGrid(gridSize) {
+  container.innerHTML = "";
+  container.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr`;
 for (let i = 0; i < gridSize; i++) {
   for (let i = 0; i < gridSize; i++) {
     let square = new Square('white')
+    }
   }
+}
+
+
+//Button functions
+
+function erase() {
+  console.log("cleared!")
+  renderGrid(gridSize);
+  
+}
+
+function clearSquares() {
+  console.log("cleared!")
+  renderGrid(gridSize);
+}
+
+function fill() {
+  squareList.forEach(function(square){
+    square.style.backgroundColor = color2;
+  })
 }
