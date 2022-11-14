4f91fce18b2df9b7f9cce3faa8b9c68b47bc7afc
Michael <michaelhein229@gmail.com>
2022-11-14 11:26:04 -0800

    Removed grid lines because it was causing error in render()

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -8,7 +8,7 @@ let squareList = [];
       this.color = color;
       this.square = document.createElement("div");
       const square = this.square
-      square.style.cssText = `background-color: ${this.color}; height: ${750 / gridSize}px; width: ${750 / gridSize}px; border: .1px solid black;`
+      square.style.cssText = `background-color: ${this.color}; height: ${750 / gridSize}px; width: ${750 / gridSize}px; `
       container.appendChild(square)
   
       square.addEventListener("mouseover", function() {
